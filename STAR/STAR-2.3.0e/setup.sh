6568102db63c7688098280d66b8f11ecc3a9ff48
Marcus Breese <marcus@breese.com>
2014-05-08 13:34:28 -0700

    added STAR (incl. FreeBSD compiling)

--- STAR/STAR-2.3.0e/setup.sh
+++ STAR/STAR-2.3.0e/setup.sh
@@ -11,7 +11,12 @@ curl -sOL https://rna-star.googlecode.com/files/STAR_2.3.0e.tgz
 tar xf STAR_2.3.0e.tgz 
 cd STAR_2.3.0e
 echo "Compiling"
+if [ $(uname) == "FreeBSD" ]; then
+patch -p1 < ../../patch.FreeBSD
+gmake STARforMac > ../build.log 2>&1
+else
 make > ../build.log 2>&1
+fi
 if [ $? != 0 ]; then
     echo "ERROR building $PROG-$VERSION!!!!"
     echo "See build.log for more details"
