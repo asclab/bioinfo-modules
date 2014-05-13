2310168a67fa4a70d9ea4c7e1c86b7d0f3ee742a
Marcus Breese <mbreese@stanford.edu>
2014-05-13 16:54:21 -0700

    adding circos for plots

--- circos/circos-0.66/setup.sh
+++ circos/circos-0.66/setup.sh
@@ -4,7 +4,7 @@ VERSION="0.66"
 SHORT="circos - circular data visualization"
 DESCRIPTION="Circos is a software package for visualizing data and information in a circular layout."
 AUTOCOPY="0"
-
+MODFILEEXTRA="module add perl/5.18.2_CABIO"
 build() {
 echo "Downloading"
 curl -OL http://www.circos.ca/distribution/circos-0.66.tgz
